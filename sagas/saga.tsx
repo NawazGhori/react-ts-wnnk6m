97300cf2dcff6a4b01948473e21b4d98e747b9a6
NawazGhori <mohammednawaz24@gmail.com>
2021-05-15 19:41:10 +0530

    fixed errors

--- sagas/saga.tsx
+++ sagas/saga.tsx
@@ -2,14 +2,14 @@ import axios from "axios";
 import {call,put,delay} from "@redux-saga/core/effects";
 
 export function *getData(){
-  delay(5000)
+  
   const res = yield call(getCountries)
   const { data } = res;
   console.log(data)
-  yield put({type:"COUNTRIES",value:data})
+  yield put({type:"COUNTRIES",value:res.data})
 
 }
 
-function *getCountries(){
+function getCountries(){
   return axios.get("https://restcountries.eu/rest/v2/all")
 };
