b9733978f33e9a157f079298b4a11f53244304fe
NawazGhori <mohammednawaz24@gmail.com>
2021-05-15 19:35:00 +0530

    1 file modified

--- sagas/saga.tsx
+++ sagas/saga.tsx
@@ -1,12 +1,15 @@
 import axios from "axios";
-import {call,put} from "@redux-saga/core/effects";
+import {call,put,delay} from "@redux-saga/core/effects";
 
 export function *getData(){
+  delay(5000)
   const res = yield call(getCountries)
-  yield put({type:"COUNTRIES",value:res})
+  const { data } = res;
+  console.log(data)
+  yield put({type:"COUNTRIES",value:data})
 
 }
 
 function *getCountries(){
   return axios.get("https://restcountries.eu/rest/v2/all")
-}
+};
